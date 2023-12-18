3ed2e07be302e4200af967d7cb2241691397f839
Marcus Koppelmann <29037335+MK2112@users.noreply.github.com>
2023-12-18 21:41:11 +0100

    moving to nodeJS

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
+<div align="center">
+<p><a href="https://twitter.com/marcus_or_so"><img src="https://img.shields.io/badge/-@marcus_or_so-555555?style=for-the-badge&amp;logo=x&amp;logoColor=white" alt="Twitter-Badge"></a> <a href="https://stackoverflow.com/users/11304860/mk2112"><img src="https://img.shields.io/badge/-MK2112-555555?style=for-the-badge&amp;logo=Stackoverflow&amp;logoColor=white" alt="Stackoverflow-Badge"></a> <img src="https://komarev.com/ghpvc/?username=MK2112&amp;style=for-the-badge" alt="Profile-Views-Count-Badge"></p>
+<hr>
 <h2>Hey there 👋</h2>
 <p align="center">
-  🔭 &nbsp; Currently working on <b>autonomous systems</b> <br>🌱 &nbsp; Improving on <b>JAX</b> and <b>Julia</b><br>💬 &nbsp; Happy to help!<br><br>
-  <img height="150em" src="https://github-readme-stats.vercel.app/api?username=MK2112&hide_border=true&count_private=true&show_icons=true&theme=dark" align = "center"/>
-  <img height="150em" src="https://github-readme-stats.vercel.app/api/top-langs?username=MK2112&show_icons=true&locale=en&layout=compact&hide_border=true&theme=dark" align = "center"/>
+<p>🔭   Currently working on <b>autonomous systems</b></p>
+<p>🌱   Improving on <b>JAX</b> and <b>Julia</b></p>
+<p>💬   Happy to help!</p>
 </p>
+<hr>
+<p><a href="https://github.com/MK2112/MK2112#gh-dark-mode-only"><img src="https://github-readme-stats.vercel.app/api?username=MK2112&amp;show_icons=true&amp;hide_border=true&amp;include_all_commits=true&amp;card_width=600&amp;custom_title=GitHub%20Open%20Source%20Stats&amp;title_color=3B7EBF&amp;text_color=FFF&amp;icon_color=3B7EBF&amp;hide=contribs&amp;show=reviews,prs_merged,prs_merged_percentage&amp;theme=transparent#gh-dark-mode-only" alt="GitHub-Stats-Card-Dark"></a></p>
+<p><a href="https://github.com/MK2112/MK2112#gh-light-mode-only"><img src="https://github-readme-stats.vercel.app/api?username=MK2112&amp;show_icons=true&amp;hide_border=true&amp;include_all_commits=true&amp;card_width=600&amp;custom_title=GitHub%20Open%20Source%20Stats&amp;title_color=3B7EBF&amp;text_color=474A4E&amp;icon_color=3B7EBF&amp;hide=contribs&amp;show=reviews,prs_merged,prs_merged_percentage&amp;theme=transparent#gh-light-mode-only" alt="GitHub-Stats-Card-Light"></a></p>
+<p><a href="https://github.com/MK2112/MK2112#gh-dark-mode-only"><img src="https://github-readme-stats.vercel.app/api/top-langs/?username=MK2112&amp;layout=compact&amp;hide_border=true&amp;card_width=600&amp;custom_title=GitHub%20Open%20Source%20Stats&amp;title_color=3B7EBF&amp;text_color=FFF&amp;icon_color=3B7EBF&amp;theme=transparent#gh-dark-mode-only" alt="GitHub-Lang-Card-Dark"></a></p>
+<p><a href="https://github.com/MK2112/MK2112#gh-light-mode-only"><img src="https://github-readme-stats.vercel.app/api/top-langs/?username=MK2112&amp;layout=compact&amp;hide_border=true&amp;card_width=600&amp;custom_title=GitHub%20Open%20Source%20Stats&amp;title_color=3B7EBF&amp;text_color=474A4E&amp;icon_color=3B7EBF&amp;theme=transparent#gh-light-mode-only" alt="GitHub-Lang-Card-Light"></a></p>
+  </div>
---
+++ src/app.ts
@@ -0,0 +1,61 @@
+import * as fs from "fs";
+const md = require("markdown-it")({
+  html: true,
+  breaks: true,
+  linkify: true
+});
+// import { fetchGitHubData } from "./fetchGitHubData";
+// ${await fetchGitHubData(githubUsername, ossProjectRepos)}
+
+const ossProjectRepos = [
+  "nn-zero-to-hero-notes",
+  "any_to_any.py",
+  "CarND"
+];
+
+const githubUsername = "MK2112";
+const twitterUrl = "https://twitter.com/marcus_or_so";
+const stackoverflowUrl = "https://stackoverflow.com/users/11304860/mk2112";
+
+async function generateMarkdown() {
+  const stackoverflowBadge = `[![Stackoverflow-Badge](https://img.shields.io/badge/-MK2112-555555?style=for-the-badge&logo=Stackoverflow&logoColor=white)](${stackoverflowUrl})`;
+  const twitterBadge = `[![Twitter-Badge](https://img.shields.io/badge/-@marcus_or_so-555555?style=for-the-badge&logo=x&logoColor=white)](${twitterUrl})`;
+  const profileCountBadge = `![Profile-Views-Count-Badge](https://komarev.com/ghpvc/?username=${githubUsername}&style=for-the-badge)`;
+
+  const githubStatsCardLight = `[![GitHub-Stats-Card-Light](https://github-readme-stats.vercel.app/api?username=${githubUsername}&show_icons=true&hide_border=true&include_all_commits=true&card_width=600&custom_title=GitHub%20Open%20Source%20Stats&title_color=3B7EBF&text_color=474A4E&icon_color=3B7EBF&hide=contribs&show=reviews,prs_merged,prs_merged_percentage&theme=transparent#gh-light-mode-only)](https://github.com/${githubUsername}/${githubUsername}#gh-light-mode-only)`;
+  const githubStatsCardDark = `[![GitHub-Stats-Card-Dark](https://github-readme-stats.vercel.app/api?username=${githubUsername}&show_icons=true&hide_border=true&include_all_commits=true&card_width=600&custom_title=GitHub%20Open%20Source%20Stats&title_color=3B7EBF&text_color=FFF&icon_color=3B7EBF&hide=contribs&show=reviews,prs_merged,prs_merged_percentage&theme=transparent#gh-dark-mode-only)](https://github.com/${githubUsername}/${githubUsername}#gh-dark-mode-only)`;
+  
+  const githubLangCardLight = `[![GitHub-Lang-Card-Light](https://github-readme-stats.vercel.app/api/top-langs/?username=${githubUsername}&layout=compact&hide_border=true&card_width=600&custom_title=GitHub%20Open%20Source%20Stats&title_color=3B7EBF&text_color=474A4E&icon_color=3B7EBF&theme=transparent#gh-light-mode-only)](https://github.com/${githubUsername}/${githubUsername}#gh-light-mode-only)`;
+  const githubLangCardDark = `[![GitHub-Lang-Card-Dark](https://github-readme-stats.vercel.app/api/top-langs/?username=${githubUsername}&layout=compact&hide_border=true&card_width=600&custom_title=GitHub%20Open%20Source%20Stats&title_color=3B7EBF&text_color=FFF&icon_color=3B7EBF&theme=transparent#gh-dark-mode-only)](https://github.com/${githubUsername}/${githubUsername}#gh-dark-mode-only)`;
+
+  const markdownText = `<div align="center">\n
+
+  ${twitterBadge} ${stackoverflowBadge} ${profileCountBadge}\n
+
+  ---\n
+
+<h2>Hey there 👋</h2>\n
+<p align="center">\n
+  🔭 &nbsp; Currently working on <b>autonomous systems</b>\n
+  🌱 &nbsp; Improving on <b>JAX</b> and <b>Julia</b>\n
+  💬 &nbsp; Happy to help!\n
+</p>\n
+
+  ---\n
+
+  ${githubStatsCardDark}\n
+  ${githubStatsCardLight}\n
+  ${githubLangCardDark}\n
+  ${githubLangCardLight}\n
+
+  </div>`;
+
+  const result = md.render(markdownText);
+
+  fs.writeFile("README.md", result, (error) => {
+    if (error) throw new Error(`Something went wrong: ${error}.`);
+    console.log(`✅ README.md file was succesfully generated.`);
+  });
+}
+
+generateMarkdown();
