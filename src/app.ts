9418fc82e5de3d9d14992774b5f06c6f48c70b61
MK2112 <29037335+MK2112@users.noreply.github.com>
2024-01-29 02:11:43 +0100

    adapted heading icons style

--- src/app.ts
+++ src/app.ts
@@ -19,10 +19,10 @@ const stackoverflowUrl = "https://stackoverflow.com/users/11304860/mk2112";
 const huggingfaceUrl = "https://huggingface.co/Marcus2112";
 
 async function generateMarkdown() {
-  const stackoverflowBadge = `[![Stackoverflow-Badge](https://img.shields.io/badge/-MK2112-555555?style=for-the-badge&logo=Stackoverflow&logoColor=white)](${stackoverflowUrl})`;
-  const huggingfaceBadge = `[![Huggingface-Badge](https://img.shields.io/badge/-Marcus2112-555555?style=for-the-badge&logo=Huggingface&logoColor=white)](${huggingfaceUrl})`;
-  const twitterBadge = `[![Twitter-Badge](https://img.shields.io/badge/-@marcus_or_so-555555?style=for-the-badge&logo=x&logoColor=white)](${twitterUrl})`;
-  const profileCountBadge = `![Profile-Views-Count-Badge](https://komarev.com/ghpvc/?username=${githubUsername}&style=for-the-badge)`;
+  const stackoverflowBadge = `[![Stackoverflow-Badge](https://img.shields.io/badge/-MK2112-grey?logo=stackoverflow&logoColor=white)](${stackoverflowUrl})`;
+  const huggingfaceBadge = `[![Huggingface-Badge](https://img.shields.io/badge/%F0%9F%A4%97-Marcus2112-grey)](${huggingfaceUrl})`;
+  const twitterBadge = `[![Twitter-Badge](https://img.shields.io/badge/-Marcus%5For_so-grey?logo=x)](${twitterUrl})`;
+  const profileCountBadge = `![Profile-Views-Count-Badge](https://komarev.com/ghpvc/?username=${githubUsername}&color=grey)`;
 
   const githubStatsCardLight = `[![GitHub-Stats-Card-Light](https://github-readme-stats.vercel.app/api?username=${githubUsername}&show_icons=true&hide_border=true&include_all_commits=true&card_width=600&custom_title=GitHub%20Open%20Source%20Stats&title_color=3B7EBF&text_color=474A4E&icon_color=3B7EBF&hide=contribs&show=reviews,prs_merged,prs_merged_percentage&theme=transparent#gh-light-mode-only)](https://github.com/${githubUsername}/${githubUsername}#gh-light-mode-only)`;
   const githubStatsCardDark = `[![GitHub-Stats-Card-Dark](https://github-readme-stats.vercel.app/api?username=${githubUsername}&show_icons=true&hide_border=true&include_all_commits=true&card_width=600&custom_title=GitHub%20Open%20Source%20Stats&title_color=3B7EBF&text_color=FFF&icon_color=3B7EBF&hide=contribs&show=reviews,prs_merged,prs_merged_percentage&theme=transparent#gh-dark-mode-only)](https://github.com/${githubUsername}/${githubUsername}#gh-dark-mode-only)`;
